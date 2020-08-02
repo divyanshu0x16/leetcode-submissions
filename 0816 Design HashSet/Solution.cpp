51a2935a8169ed6143c19be2e66e5c3cc89999d1
divyanshu0x16 <divyanshu22.dm@gmail.com>
2020-08-02 17:47:52 +0000

    Syncing - Design HashSet

---
+++ 0816 Design HashSet/Solution.cpp
@@ -0,0 +1,37 @@
+class MyHashSet {
+    int numBuckets;
+    vector< vector< int > > buckets;
+
+    int hash_function(int key){
+        return key%numBuckets;
+    }
+public:
+    /** Initialize your data structure here. */
+    /* The Constructer is done */
+    MyHashSet() {
+        numBuckets = 15000;
+        buckets = vector<vector< int > >(numBuckets, vector<int> {});   
+    }
+    
+    void add(int key) {
+        int i = hash_function(key);
+        if( find( buckets[i].begin(), buckets[i].end() , key) == buckets[i].end() )
+            buckets[i].push_back(key);
+    }
+    
+    void remove(int key) {
+        int i = hash_function(key);
+        auto it = find( buckets[i].begin(), buckets[i].end() , key);
+        if( it != buckets[i].end())
+            buckets[i].erase(it); 
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        int i = hash_function(key);
+        if( find( buckets[i].begin(), buckets[i].end() , key) == buckets[i].end() )
+            return false;
+        else 
+            return true;   
+    }
+};

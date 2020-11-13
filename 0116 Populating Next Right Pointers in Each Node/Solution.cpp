3dfa633902c48cd769a34f966de8b1b74a918de2
divyanshu0x16 <divyanshu22.dm@gmail.com>
2020-11-13 09:23:07 +0000

    Syncing - Populating Next Right Pointers in Each Node

--- 0116 Populating Next Right Pointers in Each Node/Solution.cpp
+++ 0116 Populating Next Right Pointers in Each Node/Solution.cpp
@@ -19,18 +19,17 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        Node *pre = root, *cur;
-        while (pre) {
-            cur = pre;
-            while (cur && cur -> left) { 
-                cur -> left -> next = cur -> right;
-                if (cur -> next) {
-                    cur -> right -> next = cur -> next -> left;
-                }
-                cur = cur -> next;
-            }
-            pre = pre -> left;
-        }
-        return root;
+        
+        if(!root) return root;
+        
+        if(root->left)
+            root->left->next = root->right;
+        if(root->next && root->right) 
+            root->right->next = root->next->left;
+        
+        connect(root->left);
+        connect(root->right);
+        
+        return root;        
     }
 };

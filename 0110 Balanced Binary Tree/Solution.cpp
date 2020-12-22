248e5603e1054328d64f8d8ff2f21a5eeb3effb0
divyanshu0x16 <divyanshu22.dm@gmail.com>
2020-12-22 11:39:54 +0000

    Syncing - Balanced Binary Tree

---
+++ 0110 Balanced Binary Tree/Solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ans = true;
+    int solve(TreeNode *root){
+        if(!root)
+            return 0;
+        int lh = solve(root -> left);
+        int rh = solve(root -> right);
+        if(abs(lh - rh ) > 1)
+            ans = false;
+        return 1 + max(lh,rh); 
+    }
+    bool isBalanced(TreeNode* root) {
+        solve(root);
+        return ans;
+    }
+};

84f3ea70cd86dcbf4238a81ef4516b8782aaad94
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-15 07:44:32 +0000

    Syncing - Valid Triangle Number

---
+++ 0611 Valid Triangle Number/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if(nums.size() < 3) return 0;
+        int count = 0;
+        for (int i = 0; i < nums.size() - 2; i++)
+        {
+            int k = i + 2;
+            for (int j = i+1; j < nums.size() - 1 and nums[i] != 0; j++)
+            {
+                while (k < nums.size() and nums[i] + nums[j] > nums[k])
+                    k++;
+                count += k -j -1;
+            }
+        }
+        return count;
+    }
+};

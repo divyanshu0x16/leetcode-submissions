c9780e9fe9820cd31535dbe5335261c673f2b0b8
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-07-21 04:28:53 +0000

    Syncing - Shuffle an Array

---
+++ 0384 Shuffle an Array/Solution.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    Solution(vector<int>& nums) {
+        original = nums;
+    }
+    
+    vector<int> reset() {
+        return original;
+    }
+    
+    vector<int> shuffle() {
+        vector<int> shuffled = original;
+        
+        for (int i = original.size() - 1; i >= 0; i--) {
+            int r = rand() % (i+1);
+            swap(shuffled[i], shuffled[r]);
+        }
+        
+        return shuffled;
+    }
+    
+private:
+    vector<int> original;
+};
+        
+
+    
+    /** Resets the array to its original configuration and return it. */
+ 
+        
+
+    
+    /** Returns a random shuffling of the array. */
+
+        
+
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */

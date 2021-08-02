3f4dda8fcd21beb712980ab92431af0cabaa340f
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-08-02 14:43:42 +0000

    Syncing - Two Sum

--- 0001 Two Sum/Solution.cpp
+++ 0001 Two Sum/Solution.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, vector<int> > indexes;
-        
-        for (int i = 0; i < nums.size(); i++)
-            indexes[nums[i]].push_back(i);
-
+        map<int, int> doesExist;
         vector<int> ans;
-
-        for (auto &&i : indexes)
+        for (int i = 0; i < nums.size(); i++)
         {
-            int rem = target - i.first;
-
-            if( indexes.find(rem) != indexes.end()){
-                
-                vector<int> first = i.second;
-                vector<int> second = indexes[rem];
-
-                for (int i = 0; i < first.size(); i++)
-                    for (int j = 0; j < second.size(); j++)
-                        if( first[i] != second[j]) {
-                            ans.push_back(first[i]);
-                            ans.push_back(second[j]);
-                            return ans;
-                        }
-            }
+            int curr = nums[i];
+            int rem = target - curr;
+            if( rem != curr ) doesExist[curr] = i;
+            if( (doesExist.find(rem) != doesExist.end()) and ( doesExist[rem] != i)){
+                ans.push_back(i);
+                ans.push_back(doesExist[rem]);
+                break;
+            }      
+            doesExist[curr] = i;   
         }
         return ans;
     }

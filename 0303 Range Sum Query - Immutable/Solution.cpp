c8214e1f2c55cbeff9e4f14ede73f506e663d3b6
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-08-18 14:50:21 +0000

    Syncing - Range Sum Query - Immutable

--- 0303 Range Sum Query - Immutable/Solution.cpp
+++ 0303 Range Sum Query - Immutable/Solution.cpp
@@ -1,47 +1,17 @@
 class NumArray {
 public:
-    vector<int> tree;
-    int n;
-
     NumArray(vector<int>& nums) {
-        if( nums.size() > 0){
-            n = nums.size();
-            tree = vector<int> (2*n);
-            buildTree(nums);
-        }
+        accu.push_back(0);
+        for(int num: nums)
+            accu.push_back(accu.back() + num);
     }
     
     int sumRange(int left, int right) {
-        // get leaf with value 'l'
-        left += n;
-        // get leaf with value 'r'
-        right += n;
-        int sum = 0;
-        while (left <= right)      
-        {
-            if(( left%2 ) == 1){
-                sum += tree[left];
-                left++;
-            }
-            if(( right%2) == 0){
-                sum += tree[right];
-                right--;
-            }
-            left /= 2;
-            right /= 2;
-        }
-        return sum;
+        return accu[right + 1] - accu[left];
     }
 
-private: 
-
-    void buildTree( vector<int>& nums){
-        for(int i = n, j = 0; i < 2*n; i++, j++)
-                tree[i] = nums[j];
-
-        for (int i = n-1; i > 0; i--)
-            tree[i] = tree[i*2] + tree[i*2 + 1];
-    }
+    private:
+        vector<int> accu;
 };
 
 /**

a3760a6aba2c84a2b1e6c3091f3ec33c07cf983f
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-10-12 13:05:16 +0000

    Syncing - Guess Number Higher or Lower

---
+++ 0374 Guess Number Higher or Lower/Solution.cpp
@@ -0,0 +1,19 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        while(guess(n) != 0){
+            if( guess(n) == 1 ) n++;
+            else if( guess(n) == -1) n--;
+        }
+        return n;   
+    }
+};

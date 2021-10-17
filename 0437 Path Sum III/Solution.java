393b82173b11a46acc1635b2a34c449231fa48aa
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-10-17 14:43:12 +0000

    Syncing - Path Sum III

---
+++ 0437 Path Sum III/Solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0,1);
+        helper(root, 0, sum, preSum);
+        return count;
+    }
+    int count = 0;
+    public void helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return;
+        }
+        
+        currSum += root.val;
+
+        if (preSum.containsKey(currSum - target)) {
+            count += preSum.get(currSum - target);
+        }
+        
+        if (!preSum.containsKey(currSum)) {
+            preSum.put(currSum, 1);
+        } else {
+            preSum.put(currSum, preSum.get(currSum)+1);
+        }
+        
+        helper(root.left, currSum, target, preSum);
+        helper(root.right, currSum, target, preSum);
+        preSum.put(currSum, preSum.get(currSum) - 1);
+    }
+}

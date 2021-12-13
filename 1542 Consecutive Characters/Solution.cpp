ced6b117c064c6fcd63eb9835f97e5e95c30fcc8
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-12-13 12:57:13 +0000

    Syncing - Consecutive Characters

--- 1542 Consecutive Characters/Solution.cpp
+++ 1542 Consecutive Characters/Solution.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int maxPower(string s) {
-        int power = 1;
-        int ans = 1;
-        for(int i = 1;i < s.length();i++){
-            if(s[i] == s[i - 1])
-                ans = max(ans,++power);
+        if(s.size() == 1) return 1;
+        int currMax = 0;
+        int maxLen = 0;
+        
+        for(int i=1; i<s.size(); i++) {
+            if(s[i] == s[i-1])
+                currMax++;
             else
-                power = 1;
+                currMax = 0;
+            
+            maxLen = max(maxLen, currMax+1);
         }
-        return ans;
+        
+        return maxLen;
     }
 };

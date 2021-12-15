8bb8167827c1961e012c46b96f2c9fe5d95b9794
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-12-15 06:10:02 +0000

    Syncing - Insertion Sort List

--- 0147 Insertion Sort List/Solution.cpp
+++ 0147 Insertion Sort List/Solution.cpp
@@ -11,25 +11,39 @@
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
-        if(head == nullptr){
-            return nullptr;
-        }
-        vector<ListNode*> nodes;
-        nodes.push_back(head);
-        ListNode* temp = head->next;
-        
-        while(temp != nullptr){
-            nodes.push_back(temp);
-            for(int i = nodes.size()-1; i >0; i--){
-                if( nodes[i]->val < nodes[i-1]->val ){
-                     swap(nodes[i-1]->val , nodes[i]->val);    
-                }//At each index we get the element to right position
-                else{
-                    break;
+        ListNode* newHead = NULL;//initializing the newHead for our sorted linkedlist
+        while(head){
+            // Exluding node from the original linked list we will do this one at a time
+            ListNode* temp = head;
+            head = head->next;
+            temp->next=NULL;
+            
+            //setting the first node of our final linked list 
+            if(newHead == NULL) newHead = temp;
+             // if the position of element is at index 0 i.e. at the start (the temp node is the smallest of all the nodes that are currently present in the sorted linked list)
+            else if(newHead->val >= temp->val){
+                temp->next = newHead;
+                newHead = temp;
+            }
+            // inserting the node anywhere in the middle or in the end depending upon the value of the temp node;
+            else{
+                ListNode* root = newHead;
+                {
+                while(root->next){
+                    if(temp->val > root->val and temp->val <= root->next->val){
+                        temp->next = root->next;
+                        root->next = temp;
+                        break;
+                    }
+                    root = root->next;
+                }  
+                    //inserting the temp node at the end
+                    if(root->next==NULL) root->next = temp;
+                    
                 }
             }
-            temp = temp -> next;
         }
-        return head;
+        //Our sorted linkedlist
+        return newHead;
     }
 };

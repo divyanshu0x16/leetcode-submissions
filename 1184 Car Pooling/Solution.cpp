b03e03656405652da874b7aedccd1e53ffd98280
divyanshu0x16 <divyanshu22.dm@gmail.com>
2022-01-06 05:23:10 +0000

    Syncing - Car Pooling

---
+++ 1184 Car Pooling/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<int> stops(1001, 0);
+        for(auto t: trips){
+            stops[t[1]] += t[0];
+            stops[t[2]] -= t[0];
+        }
+
+        for(auto i = 0; capacity >= 0 and i < 1001; ++i){
+            capacity -= stops[i];
+        }
+
+        return capacity >= 0;
+    }
+};

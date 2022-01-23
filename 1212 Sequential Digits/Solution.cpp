8c756607062f0fb86c01bfd313da505cef2d916d
divyanshu0x16 <divyanshu22.dm@gmail.com>
2022-01-23 13:49:18 +0000

    Syncing - Sequential Digits

---
+++ 1212 Sequential Digits/Solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        queue<int> q;
+        for (int i = 1; i <= 9; i++){
+            q.push(i);
+        }
+
+        vector<int> ret;
+
+        while (!q.empty()){
+            int f = q.front();
+            q.pop();
+
+            if( f <= high and f >= low){
+                ret.push_back(f);
+            }
+
+            if(f > high) break;
+
+            int num = f%10;
+
+            if(num < 9){
+                q.push(f*10 + (num+1));
+            }
+        }
+        return ret;
+    }
+};

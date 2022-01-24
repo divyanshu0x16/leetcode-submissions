db3cde1b79e3e6e0af38f143a7abc37a5a186801
divyanshu0x16 <divyanshu22.dm@gmail.com>
2022-01-24 12:22:22 +0000

    Syncing - Detect Capital

--- 0520 Detect Capital/Solution.cpp
+++ 0520 Detect Capital/Solution.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        bool flag = true;
-        if( !isupper( word[0] ) ){
-            for (int i = 1; i < word.length() ; i++)
-            {
-                if( isupper(word[i]) ) {
-                    flag = false;
-                    break;
-                }
-            }
-        }else{
-            if( isupper( word[1] ) ){
-                for (int i = 2; i < word.length(); i++){
-                    if( islower( word[i] ) ){
-                        flag = false;
-                        break;
-                    }
-                }
-            }else{
-                for (int i = 1; i < word.length(); i++){
-                    if( isupper( word[i]) ){
-                        flag = false;
-                        break;
-                    }
-                }
+        int cnt=0;
+        int loc;
+        for(int i=0;i<word.length();i++){
+            if(word[i]>='A' && word[i]<='Z'){
+                cnt++;
+                loc = i;
             }
         }
-        return flag;  
+        if(cnt == word.length() || cnt == 0 || (cnt==1 && loc==0))
+            return true;
+        return false;
     }
-};
+};   

44ff1b84ed2454667e4a709dbcfb2b7fe69d8825
divyanshu0x16 <divyanshu22.dm@gmail.com>
2022-05-25 14:32:26 +0000

    Syncing - Longest Increasing Subsequence

--- 0300 Longest Increasing Subsequence/Solution.cpp
+++ 0300 Longest Increasing Subsequence/Solution.cpp
@@ -7,7 +7,7 @@ public:
         for (int i = 1; i < n; i++)
         {
             dp[i] = 1;
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < i; j++)
             {
                 if( (nums[i] > nums[j]) and dp[i] < dp[j] + 1)
                     dp[i] = dp[j] + 1;

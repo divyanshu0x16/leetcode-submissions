1eb584df07de241e32c99e24ea968834be1ddc4a
divyanshu0x16 <divyanshu22.dm@gmail.com>
2022-08-04 06:05:44 +0000

    Syncing - Valid Palindrome

--- 0125 Valid Palindrome/Solution.cpp
+++ 0125 Valid Palindrome/Solution.cpp
@@ -1,12 +1,27 @@
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(string s) {
-        int j = s.length()-1,i=0;
-        while(i<=j){
-            while(i<j&&!isalpha(s[i])&&!isdigit(s[i]))i++;    //can be done by while(i<j&&!isalnum(s[i]))i++;  <- Credit @bhawan0407 for this
-            while(i<j&&!isalpha(s[j])&&!isdigit(s[j]))j--;
-            if(tolower(s[i])==tolower(s[j]))i++,j--;
-            else return false;
+    bool isPalindrome(string s)
+    {
+        // convert string to lowercase and remove alphanumeric characters
+
+        for (int i = 0; i < s.length(); i++)
+            s[i] = tolower(s[i]);
+        s.erase(remove(s.begin(), s.end(), ' '), s.end()); // to remove spaces
+        s.erase(std::remove_if(s.begin(), s.end(),
+                               [](auto const &c) -> bool
+                               { return !std::isalnum(c); }),
+                s.end());
+
+        // check if it is a palindrome using two pointers
+        for (int i = 0, j = s.length() - 1; i < s.length(), j >= 0; i++, j--)
+        {
+            if (i > j)
+                break;
+            if (s[i] != s[j])
+            {
+                return false;
+            }
         }
         return true;
     }

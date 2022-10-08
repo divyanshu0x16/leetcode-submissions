e4f458cace9c59a427d18808d93d9d9d25dae8d2
divyanshu0x16 <divyanshu22.dm@gmail.com>
2022-10-08 11:16:11 +0000

    Syncing - Pascal's Triangle

--- 0118 Pascal's Triangle/Solution.cpp
+++ 0118 Pascal's Triangle/Solution.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector< vector < int > > triangle;
 
-        vector<int> hello; hello.push_back(1);
-        triangle.push_back(hello);
+		if( numRows == 1 ) {
+			vector<vector<int> > pascalTraingle = {{1}};
+			return pascalTraingle;
+		}
 
-        for (int i = 1; i < numRows; i++)
-        {
-            vector<int> row;
-            vector<int> prevRow = triangle[i-1];
+		if( numRows == 2 ){
+			vector<vector<int> > pascalTraingle = {{1}, {1, 1}};
+			return pascalTraingle;
+		}
+        
+		vector<vector<int> > pascalTraingle = {{1}, {1, 1}};
 
-            row.push_back(1);
+		int currRows = 2;
 
-            for (int j = 1; j < i; j++)
-                row.push_back( prevRow[j-1] + prevRow[j] );
+		while (currRows < numRows)
+		{
+			vector<int> currentRow;
+			vector<int> previousRow = pascalTraingle[pascalTraingle.size()-1];
+			currentRow.push_back(1);
 
-            row.push_back(1);
-            triangle.push_back(row);
-        }
-        return triangle;
+			while (currentRow.size() < previousRow.size())
+				for (int i = 0; i <= previousRow.size() - 2; i++)
+					currentRow.push_back( previousRow[i] + previousRow[i+1] );
+			
+			currentRow.push_back(1);
+			currRows++;
+
+			pascalTraingle.push_back(currentRow);
+		}
+		
+		return pascalTraingle;
     }
 };
